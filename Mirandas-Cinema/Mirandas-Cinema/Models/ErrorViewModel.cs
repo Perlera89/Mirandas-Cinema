36ff070bb5213bf0f676f708e7d83a1d35c2e47f
Manuel <69605277+Perlera89@users.noreply.github.com>
2021-10-23 14:05:37 -0600

    Agregando Modelos

---
+++ Mirandas-Cinema/Mirandas-Cinema/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mirandas_Cinema.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
