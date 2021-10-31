64c82e83a97cce7d2d0dc47afdd4b36a1c3a9aa6
Manuel <69605277+Perlera89@users.noreply.github.com>
2021-10-31 17:26:26 -0600

    agregando recursos

--- Mirandas-Cinema/Mirandas-Cinema/Data/AppDbInicializer.cs
+++ Mirandas-Cinema/Mirandas-Cinema/Data/AppDbInicializer.cs
@@ -26,19 +26,19 @@ namespace Mirandas_Cinema.Data
                         new Cinema()
                         {
                             Name = "Miranda's Home",
-                            Logo = "https://i.pinimg.com/originals/3f/11/82/3f11820bf22ebb3dbc57679db3591c77.png",
+                            Logo = "/resources/cinemas/home.png",
                             Description = "Aqui va la descripcion del primer cine"
                         },
                         new Cinema()
                         {
                             Name = "Miranda's Gold",
-                            Logo = "https://www.kindpng.com/picc/m/731-7317749_golden-cinema-logo-hd-png-download.png",
+                            Logo = "/resources/cinemas/gold.png",
                             Description = "Aqui va la descripcion del segundo cine"
                         },
                         new Cinema()
                         {
                             Name = "Miranda's Express",
-                            Logo = "https://yt3.ggpht.com/ytc/AKedOLRucw4LUwG-MRu8ndydJvXJ711Uhcf2fsId7PaQ=s900-c-k-c0x00ffffff-no-rj",
+                            Logo = "/resources/cinemas/express.png",
                             Description = "Aqui va la descripcion del tercer cine"
                         }
                         });
@@ -57,35 +57,35 @@ namespace Mirandas_Cinema.Data
                             Bio = "es un productor y director de televisión británico, quizás más conocido por sus colaboraciones " +
                             "con Armando Iannucci. Como tal, ha trabajado en The Saturday Night Armistice, The Armando Iannucci Shows, " +
                             "Time Trumpet y The Thick of It.",
-                            ProfilePictureURL = "http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQlgLaet3rZfarinMI9tfWymfJrhiU_apKwN7K-IX-nZDorGhE5n6qi0Y0Du0Dx"
+                            ProfilePictureURL = "/resources/producers/adam-tandy.jpg"
 
                         },
                         new Producer()
                         {
                             FullName = "Chris Carreras",
                             Bio = "Mejor conocido como Asistente de Dirección basado en créditos en ese papel en 33 películas",
-                            ProfilePictureURL = "https://images.mubicdn.net/images/cast_member/161192/cache-485869-1573127753/image-w856.jpg?size=160x"
+                            ProfilePictureURL = "/resources/producers/chris-carreras.jpg"
                         },
                         new Producer()
                         {
                             FullName = "Aaron Fjellman",
                             Bio = "Aaron Fjellman es un escritor y director estadounidense. Recientemente, hizo su debut cinematográfico " +
                             "con Caged, un thriller psicológico sobre los horribles y alucinantes efectos del confinamiento solitario.",
-                            ProfilePictureURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIkljqLrJRAGcrj2Gf1zZ4hXhhtikr4yriMgmpTTc3xjpin-w0"
+                            ProfilePictureURL = "/resources/producers/aaron-fjellman.jpg"
                         },
                         new Producer()
                         {
                             FullName = "Toño Lopez",
                             Bio = "Toño López nació en 1971 en Ourense, Galicia, España. Es director y editor, conocido por Matalobos (2009), " +
                             "4º sen ascensor (2005) y La revoltosa (2010).",
-                            ProfilePictureURL = "http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcQZw6BHUNY6-3pqIokXwKWpAcR7Rt8z6X5qWuqO96tn7ni9jOk4NHJBl__a3dSY"
+                            ProfilePictureURL = "/resources/producers/tonio-lopez.jpg"
                         },
                         new Producer()
                         {
                             FullName = "Brian Baugh",
                             Bio = "Brian Baugh es un director, guionista y productor estadounidense conocido por su compromiso de contar historias humanas " +
                             "inspiradoras y socialmente relevantes que abarcan géneros.",
-                            ProfilePictureURL = "https://m.media-amazon.com/images/M/MV5BYjY0MDBjZDMtNTA2Yy00ZDA0LTk5ZmUtZTQ1OTM3OTRiYmU2XkEyXkFqcGdeQXVyMjQwMDg0Ng@@._V1_.jpg"
+                            ProfilePictureURL = "/resources/producers/brian-baugh.jpg"
                         }
                     });
 
@@ -103,7 +103,7 @@ namespace Mirandas_Cinema.Data
                             Bio = "Katherine McNamara, nombrada una de las 'Breakout Bunch' de Vanity Fair, es una actriz, bailarina, cantante y compositora" +
                             " consumada y acaba de recibir el premio People's Choice Award de 2018 como Mejor Actriz de Televisión Femenina por su papel principal " +
                             "de 'Clary Fray' en la serie Freeform",
-                            ProfilePictureURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Katherine_McNamara_2014_%28cropped%29.jpg/1200px-Katherine_McNamara_2014_%28cropped%29.jpg"
+                            ProfilePictureURL = "/resources/actors/katerine-mcNamara.jpg"
 
                         },
                         new Actor()
@@ -111,27 +111,27 @@ namespace Mirandas_Cinema.Data
                             FullName = "Timothée Chalamet",
                             Bio = "Timothée Hal Chalamet nació en Manhattan, de Nicole Flender, una corredora de bienes raíces y bailarina, y Marc Chalamet, " +
                             "un editor de UNICEF. Su madre, que es de Nueva York, es judía, de ascendencia judía rusa y judía austriaca.",
-                            ProfilePictureURL = "https://th.bing.com/th/id/R.c00691ba9adbeb611b85cd2732486cdd?rik=FK6nGoEN2PlaZQ&pid=ImgRaw&r=0"
+                            ProfilePictureURL = "/resources/actors/timothee-chalamet.jpg"
                         },
                         new Actor()
                         {
                             FullName = "Jack Dylan Grazer",
                             Bio = "Jack Dylan Grazer nació en Los Ángeles, California, de padres Angela LaFever y Gavin Grazer. Al crecer en el mundo del entretenimiento " +
                             "y sus alrededores, no pasó mucho tiempo antes de que se descubriera el talento de Jack y se le animara a intentar actuar profesionalmente.",
-                            ProfilePictureURL = "https://es.web.img3.acsta.net/c_310_420/pictures/19/03/27/23/41/0862266.jpg"
+                            ProfilePictureURL = "/resources/actors/jack-dylan-grazer.jpg"
                         },
                         new Actor()
                         {
                             FullName = "María Vázquez",
                             Bio = "María Vázquez nació el 19 de marzo de 1979 en Vigo, Pontevedra, España como María Rodríguez Vázquez. Es actriz, conocida por Mataharis (2007), " +
                             "Quien a hierro mata (2019) y Trote (2018).",
-                            ProfilePictureURL = "http://t2.gstatic.com/licensed-image?q=tbn:ANd9GcRLL7hFMSEU0jcesd_mPQQEWL3aQDXdF3YhEkvK8tvyCOWwvIf7RCHlthdUfISO"
+                            ProfilePictureURL = "/resources/actors/maria-vasquez.jpg"
                         },
                         new Actor()
                         {
                             FullName = "Edi Gathegi",
                             Bio = "Edi Gathegi nació el 10 de marzo de 1979 en Nairobi, Kenia como Edi Mue Gathegi. Es actor y productor, conocido por StartUp (2016),",
-                            ProfilePictureURL = "https://musicimage.xboxlive.com/catalog/video.contributor.9c1f6500-0200-11db-89ca-0019b92a3933/image?locale=es-es&target=circle"
+                            ProfilePictureURL = "/resources/actors/edi-gathegi.jpg"
                         }
                     });
                     context.SaveChanges();
@@ -149,7 +149,7 @@ namespace Mirandas_Cinema.Data
                             " que salva un planeta rico en especias que se le encomienda proteger. Nueva adaptación al cine de las " +
                             "novelas de Frank Herbert.",
                             Price = 19.50,
-                            ImagenURL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/ymTPj60S2T9HoQIsh4VBLu3jDM5.jpg",
+                            ImagenURL = "/resources/movies/dune.jpg",
                             StartDate = DateTime.Now.AddDays(-5),
                             EndDate = DateTime.Now.AddDays(10),
                             CinemaId = 2,
@@ -162,7 +162,7 @@ namespace Mirandas_Cinema.Data
                             Description = "Ambientada en un mundo donde los robots se han convertido en los mejores amigos de los niños, " +
                             "Ron’s Gone Wrong cuenta la historia de un niño de 11 años que descubre que su amigo robot no funciona del todo.",
                             Price = 29.50,
-                            ImagenURL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/9I3A0EMeRiiMdvO8I5GQa67hYUU.jpg",
+                            ImagenURL = "/resources/movies/ron-da-error.jpg",
                             StartDate = DateTime.Now,
                             EndDate = DateTime.Now.AddDays(3),
                             CinemaId = 1,
@@ -175,7 +175,7 @@ namespace Mirandas_Cinema.Data
                             Description = "Finley, una talentosa aspirante a violinista, conoce a Beckett, una joven estrella de cine, cuando está en " +
                             "Irlanda en su semestre de universidad fuera del país. Entre ellos surge un romance inesperado. Una pelicula llena de romance.",
                             Price = 39.50,
-                            ImagenURL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/9uopgvuSrjnr9LaIEXeZOjG20Es.jpg",
+                            ImagenURL = "/resources/movies/finding-you.jpg",
                             StartDate = DateTime.Now,
                             EndDate = DateTime.Now.AddDays(7),
                             CinemaId = 3,
@@ -189,7 +189,7 @@ namespace Mirandas_Cinema.Data
                             " de sus negocios y necesitan dinero. Mucho dinero. Será Sabonis quien encuentre la solución al problema secuestrando" +
                             " a Modesto (Federico Pérez Rey).",
                             Price = 19.50,
-                            ImagenURL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/34mk9BfGmIjERETs4JLBhS6ifxr.jpg",
+                            ImagenURL = "/resources/movies/cuniados.jpg",
                             StartDate = DateTime.Now.AddDays(-10),
                             EndDate = DateTime.Now.AddDays(-5),
                             CinemaId = 2,
@@ -203,7 +203,7 @@ namespace Mirandas_Cinema.Data
                             "después de ser declarado culpable de asesinar a su esposa, ya que es perseguido por demonios " +
                             "internos y su esposa muerta, y empujado al límite por una guardia abusiva.",
                             Price = 29.50,
-                            ImagenURL = "https://image.tmdb.org/t/p/w185_and_h278_bestv2/gkBGXYNXWhcW7VI9POpeLLNgtSd.jpg",
+                            ImagenURL = "/resources/movies/enjaulado.jpg",
                             StartDate = DateTime.Now.AddDays(-10),
                             EndDate = DateTime.Now.AddDays(-2),
                             CinemaId = 1,
