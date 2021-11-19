bd7f5f59b289b3a35958b0514b9122a28c99df3c
Wilian-Miranda <69541095+Wilian-Miranda@users.noreply.github.com>
2021-11-19 10:55:07 -0600

    Archivos estaticos para el carousel
    archivos js para los carouseles
    archivos css para los carouseles
    -Media queries

---
+++ Mirandas-Cinema/Mirandas-Cinema/wwwroot/js/jsmovies/js_movies_carousel.js
@@ -0,0 +1,66 @@
+﻿const fila = document.querySelector('.contenedor_carousel1 .carousel1');
+const pelicula = document.querySelectorAll('.pelicula1');
+
+const flechaIzquierda = document.getElementById('flecha_izquierda1');
+const flechaDerecha = document.getElementById('flecha_derecha1');
+
+/*-------flecha derecha------*/
+flechaDerecha.addEventListener('click', () => {
+    fila.scrollLeft += fila.offsetWidth;
+
+    const indicadorActivo = document.querySelector('.indicadores1 .activo');
+    //preguntando si hay un elemento a la derecha
+    if (indicadorActivo.nextSibling) {
+        indicadorActivo.nextSibling.classList.add('activo');
+        indicadorActivo.classList.remove('activo');
+    }
+});
+
+flechaIzquierda.addEventListener('click', () => {
+
+    fila.scrollLeft -= fila.offsetWidth;
+    const indicadorActivo = document.querySelector('.indicadores1 .activo');
+    //preguntando si hay un elemento a la derecha
+    if (indicadorActivo.previousSibling) {
+        indicadorActivo.previousSibling.classList.add('activo');
+        indicadorActivo.classList.remove('activo');
+    }
+});
+
+/*------paginacion------*/
+const numeroPaginas = Math.ceil(pelicula.length / 5);
+for (let i = 0; i < numeroPaginas; i++) {
+    const indicador = document.createElement('button');
+
+    if (i === 0) {
+        indicador.classList.add('activo');
+    }
+
+    document.querySelector('.indicadores1').appendChild(indicador);
+    indicador.addEventListener('click', (e) => {
+        fila.scrollLeft += i * fila.offsetWidth;
+
+        document.querySelector('.indicadores1 .activo').classList.remove('activo');
+        /*el botton clickeado*/
+        e.target.classList.add('activo');
+    });
+}
+//hover
+
+pelicula.forEach((pelicula) => {
+    pelicula.addEventListener('mouseenter', (e) => {
+        const elemento = e.currentTarget;
+        setTimeout(() => {
+            for (var i = 0; i < pelicula; i++) {
+                pelicula => pelicula.classList.remove('hover');
+            }
+
+            elemento.classList.add('hover');
+        }, 300);
+    });
+});
+
+fila.addEventListener('mouseleave', () => {
+    pelicula.forEach(pelicula => pelicula.classList.remove('hover'));
+
+});
---
+++ Mirandas-Cinema/Mirandas-Cinema/wwwroot/js/jsmovies/js_movies_carousel2.js
@@ -0,0 +1,66 @@
+﻿const fila2 = document.querySelector('.contenedor_carousel2 .carousel2');
+const pelicula2 = document.querySelectorAll('.pelicula2');
+
+const flechaIzquierda2 = document.getElementById('flecha_izquierda2');
+const flechaDerecha2 = document.getElementById('flecha_derecha2');
+
+/*-------flecha derecha------*/
+flechaDerecha2.addEventListener('click', () => {
+    fila2.scrollLeft += fila2.offsetWidth;
+
+    const indicadorActivo = document.querySelector('.indicadores2 .activo');
+    //preguntando si hay un elemento a la derecha
+    if (indicadorActivo.nextSibling) {
+        indicadorActivo.nextSibling.classList.add('activo');
+        indicadorActivo.classList.remove('activo');
+    }
+});
+
+flechaIzquierda2.addEventListener('click', () => {
+
+    fila2.scrollLeft -= fila2.offsetWidth;
+    const indicadorActivo = document.querySelector('.indicadores2 .activo');
+    //preguntando si hay un elemento a la derecha
+    if (indicadorActivo.previousSibling) {
+        indicadorActivo.previousSibling.classList.add('activo');
+        indicadorActivo.classList.remove('activo');
+    }
+});
+
+/*------paginacion------*/
+const numeroPaginas2 = Math.ceil(pelicula2.length / 5);
+for (let i = 0; i < numeroPaginas2; i++) {
+    const indicador = document.createElement('button');
+
+    if (i === 0) {
+        indicador.classList.add('activo');
+    }
+
+    document.querySelector('.indicadores2').appendChild(indicador);
+    indicador.addEventListener('click', (e) => {
+        fila2.scrollLeft += i * fila2.offsetWidth;
+
+        document.querySelector('.indicadores2 .activo').classList.remove('activo');
+        /*el botton clickeado*/
+        e.target.classList.add('activo');
+    });
+}
+//hover
+
+pelicula2.forEach((pelicula2) => {
+    pelicula2.addEventListener('mouseenter', (e) => {
+        const elemento = e.currentTarget;
+        setTimeout(() => {
+            for (var i = 0; i < pelicula2; i++) {
+                pelicula2 => pelicula2.classList.remove('hover');
+            }
+
+            elemento.classList.add('hover');
+        }, 300);
+    });
+});
+
+fila2.addEventListener('mouseleave', () => {
+    pelicula2.forEach(pelicula2 => pelicula2.classList.remove('hover'));
+
+});
